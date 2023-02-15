Advected dorsal mask, fixed units issue, new myosin dataset

atlas_processing/anisotropy_detection.py
@@ -9,7 +9,10 @@ from scipy.io import loadmat
 
 from scipy import ndimage
 from skimage import transform
-import atlas_processing.windowed_radon as wr
+try:
+	import atlas_processing.windowed_radon as wr
+except:
+	import windowed_radon as wr
 
 
 from skimage.morphology import erosion, dilation, disk

atlas_processing/atlas_processing.py
@@ -13,7 +13,8 @@ from skimage.transform import resize
 from tqdm import tqdm
 from scipy.io import loadmat
 
-basedir = '/project/vitelli/jonathan/REDO_fruitfly/src/data'
+from anisotropy_detection import collect_anisotropy_tensor
+
 
 def convert_matstruct_to_csv(savedir, prefix='dynamic'):
 	data = loadmat(os.path.join(savedir, '%s_queried_sample.mat' % prefix))
@@ -43,46 +44,85 @@ Pulling and reformatting data from Atlas
 '''
 
 from scipy.interpolate import RectBivariateSpline
+from plyfile import PlyData
 
-piv_geometry = loadmat('/project/vitelli/jonathan/REDO_fruitfly/flydrive.synology.me/minimalData/Atlas_Data/embryo_geometry/embryo_rectPIVscale_fundamentalForms.mat')
-pix_geometry = loadmat('/project/vitelli/jonathan/REDO_fruitfly/flydrive.synology.me/minimalData/vitelli_sharing/pixel_coordinates.mat')
-Xpiv, Ypiv = piv_geometry['X0'][0], piv_geometry['Y0'][:, 0]
-Xpix, Ypix = pix_geometry['XX'][0], pix_geometry['YY'][:, 0]
+atlas_dir = '/project/vitelli/jonathan/REDO_fruitfly/flydrive.synology.me/Public/dynamic_atlas'
+basedir = os.path.join(atlas_dir, 'Atlas_Data')
+geometry_dir = os.path.join(atlas_dir, 'embryo_geometry')
 
-def collect_velocity_fields(savedir):
+def collect_velocity_fields(savedir, subdir='PIV_filtered', prefix='VeloT_medfilt'):
 	'''
 	We are LOCKING IN to IJ ordering
 	Spatial ordering is [ROWS, COLUMNS] or [Y, X]
 	Channel ordering is [VY, VX] corresponding to the spatial ordering
+
+	Note that PIV_Filtered folders ALREADY do this! The VX, VY are mislabeled
+		They correspond to axes 1, 2, which are the Y, X axes
 	
 	Remember this when visualizing and lifting to 3D space
+
+	Adjust coordinate systems! The PIV_filtered folder is in units 
+		of PIV pixels / min
+	The PIV image size is 0.4 x [original dimensions], so each PIV pixel is
+		equivalent to 2.5 x original pixel size
+	The original units are 1pix=0.2619 um, so 1 PIV pix = 0.65479 um
+
+	The extent in PIV coordinates is [820, 696] 
 	'''
 	if not os.path.exists(os.path.join(savedir, 'dynamic_index.csv')):
 		warnings.warn('Index does not exist, processing matstruct')
 		convert_matstruct_to_csv(savedir)
 		
 	warnings.warn('Collecting flow fields')
+
+	#PIV_filtered units conversion
+	original_pixel_size = 0.2619
+	piv_rescale_factor = 0.4
+	piv_pixel_size = original_pixel_size / piv_rescale_factor
+
+	#Original PIV_filtered coordinates
+	piv_scale = loadmat(os.path.join(geometry_dir, 'embryo_rectPIVscale_fundamentalForms.mat'))
+	Xpiv, Ypiv = piv_scale['X0'][0], piv_scale['Y0'][:, 0]
+
+	#Establish target coordinate system in PIV coordinates
+	rect_scale = PlyData.read(os.path.join(geometry_dir, 'rect_PIVImageScale.ply'))['vertex']
+	xmin, xmax = np.min(rect_scale['x']), np.max(rect_scale['x'])
+	ymin, ymax = np.min(rect_scale['y']), np.max(rect_scale['y'])
+	nAP, nDV = 200, 236
+	ap_space = np.linspace(xmin+xmax/nAP*0.5, xmax-xmin/nAP*0.5, nAP)
+	dv_space = np.linspace(ymin+ymax/nDV*0.5, ymax-ymin/nDV*0.5, nDV)
+	Ypix, Xpix = np.meshgrid(dv_space, ap_space, indexing='ij')
 	
 	df =  pd.read_csv(os.path.join(savedir, 'dynamic_index.csv'))
 	for folder in df.folder.unique():
 		ss = df[df.folder == folder]
 		eID = ss.embryoID.iloc[0]
-		vel_dir = os.path.join(folder, 'PIV_filtered')
-		vels = []
-		for eIdx in tqdm(sorted(ss.eIdx)):
-			fn = os.path.join(vel_dir, 'VeloT_medfilt_%06d.mat' % (eIdx+1))
-			vel = loadmat(fn)
-			vx = RectBivariateSpline(Xpiv, Ypiv, vel['VX'])(Xpix, Ypix).T
-			vy = RectBivariateSpline(Xpiv, Ypiv, vel['VY'])(Xpix, Ypix).T
-			vels.append(np.stack([vx, vy]))
-		vels = np.stack(vels)
-		np.save(os.path.join(folder, 'velocity2D'), vels)
 
+		#PIV_filtered folder
+		vel_dir = os.path.join(folder, 'PIV_filtered')
+
+		vels = []
+		if os.path.exists(vel_dir):
+			for eIdx in tqdm(sorted(ss.eIdx)):
+				fn = os.path.join(vel_dir, 'VeloT_medfilt_%06d.mat' % (eIdx+1))
+				vel = loadmat(fn)
+				vx = RectBivariateSpline(Xpiv, Ypiv, vel['VX'])(ap_space, dv_space).T
+				vy = RectBivariateSpline(Xpiv, Ypiv, vel['VY'])(ap_space, dv_space).T
+				vels.append(np.stack([vx, vy]))
+			vels = np.stack(vels)
+			#vels = vels * piv_pixel_size
+			np.save(os.path.join(folder, 'velocity2D'), vels)
+		elif os.path.exists(os.path.join(folder, '%s_velocity.mat' % eID)):
+			pass
+
+		#Save coordinate system in MICRONS
+		np.save(os.path.join(folder, 'DV_coordinates'), Ypix * piv_pixel_size)
+		np.save(os.path.join(folder, 'AP_coordinates'), Xpix * piv_pixel_size)
 
 '''
 Build static ensembled timeline
 '''
-def build_ensemble_timeline(savedir, t_min=0, t_max=50, init_unc=3, sigma=3):
+def build_ensemble_timeline(savedir, t_min=0, t_max=50, init_unc=3, sigma=3, drop_times=False):
 	warnings.warn('Computing ensemble-averaged quantities for %s' % savedir)
 
 	df = pd.DataFrame()
@@ -92,6 +132,16 @@ def build_ensemble_timeline(savedir, t_min=0, t_max=50, init_unc=3, sigma=3):
 	if os.path.exists(os.path.join(savedir, 'dynamic_index.csv')):
 		df = pd.concat([df, pd.read_csv(os.path.join(savedir, 'dynamic_index.csv'))]).drop_duplicates()
 		print('Found dynamic index')
+	
+	if drop_times:
+		df.time = df.eIdx
+
+	if os.path.exists(os.path.join(savedir, 'morphodynamic_offsets.csv')):
+			morpho = pd.read_csv(os.path.join(savedir, 'morphodynamic_offsets.csv'), index_col='embryoID')
+			for eId in df.embryoID.unique():
+				df.loc[df.embryoID == eId, 'time'] -= morpho.loc[eId, 'offset']
+
+
 	print('Building ensemble movies from %d to %d' % (t_min, t_max))
 
 	outdir = os.path.join(savedir, 'ensemble')
@@ -99,6 +149,8 @@ def build_ensemble_timeline(savedir, t_min=0, t_max=50, init_unc=3, sigma=3):
 		os.mkdir(outdir)
 	
 	movies = {'t': []}
+	ap_coordinates=None
+	dv_coordinates=None
 
 	for t in tqdm(range(t_min, t_max)):
 		max_unc, flag = init_unc, True
@@ -115,7 +167,7 @@ def build_ensemble_timeline(savedir, t_min=0, t_max=50, init_unc=3, sigma=3):
 		for i, row in matches.iterrows():
 			eframe = {}
 			#Iterate over pre-computed movies 
-			for npy in glob.glob(os.path.join(row.folder, '*2D.npy')):
+			for npy in glob.glob(os.path.join(glob.escape(row.folder), '*2D.npy')):
 				name = os.path.basename(npy)[:-4]
 				mov = np.load(npy, mmap_mode='r')
 				if row.eIdx < mov.shape[0]:
@@ -143,6 +195,9 @@ def build_ensemble_timeline(savedir, t_min=0, t_max=50, init_unc=3, sigma=3):
 					frame[key] = frame[key] + weight*eframe[key]
 				else:
 					frame[key] = weight*eframe[key]
+
+			ap_coordinates = np.load(os.path.join(row.folder, 'AP_coordinates.npy'), mmap_mode='r')
+			dv_coordinates = np.load(os.path.join(row.folder, 'DV_coordinates.npy'), mmap_mode='r')
 	
 		for key in frame:
 			frame[key] = frame[key] / total_weight
@@ -154,17 +209,19 @@ def build_ensemble_timeline(savedir, t_min=0, t_max=50, init_unc=3, sigma=3):
 
 	for key in movies:
 		np.save(os.path.join(outdir, key), np.stack(movies[key]))
+	np.save(os.path.join(outdir, 'AP_coordinates'), ap_coordinates)
+	np.save(os.path.join(outdir, 'DV_coordinates'), dv_coordinates)
 
 if __name__=='__main__':
-
 	savedirs = [
-		#'WT/ECad-GFP',
+		'WT/ECad-GFP',
+		'Halo_Hetero_Twist[ey53]_Hetero/Sqh-GFP',
 		#'WT/sqh-mCherry',
 		#'WT/moesin-GFP',
 		#'WT/utr-mCherry',
 		#'WT/Sqh_RokK116A-GFP',
 		#'WT/histone-RFP',
-		'WT/Tartan/',
+		#'WT/Tartan/',
 		#'WT/Bazooka-GFP',
 		#'WT/Runt',
 		#'WT/Even_Skipped',
@@ -176,13 +233,25 @@ if __name__=='__main__':
 		#'TollRM9/Spaghetti_Squash-GFP',
 		#'optoRhoGEF2_sqhCherry/headIllumination',
 		#'optoRhoGEF2_sqhCherry/singlePlaneIllumination'
+		#'WT/sqh-GFP_twistPlus',
 	]
 
 	for savedir in savedirs:
 		fulldir = os.path.join(basedir, savedir)
-		df = convert_matstruct_to_csv(fulldir, prefix='static')
-		#print(fulldir, len(df))
-		build_ensemble_timeline(fulldir, init_unc=1)
+		print(savedir)
+		'''
+		Static datasets
+		'''
+		#df = convert_matstruct_to_csv(fulldir, prefix='static')
+		#build_ensemble_timeline(fulldir, init_unc=1)
+
+		'''
+		Dynamic datasets
+		'''
+		df = convert_matstruct_to_csv(fulldir, prefix='dynamic')
+		print(fulldir)# len(df))
 		#collect_velocity_fields(fulldir)
 		#collect_anisotropy_tensor(fulldir)
-		#push_to_embryo_surface(fulldir)
+		build_ensemble_timeline(fulldir, init_unc=1, 
+			t_min=-10, t_max=40,
+			drop_times='Sqh' in savedir)

library_generation/scalar_library.py
@@ -22,8 +22,8 @@ from scipy.ndimage import gaussian_filter
 Generate terms from scalar fields
 '''
 
-def s2s_terms(x, group, key='Rnt'):
-	d1_x, d2_x = validate_key_and_derivatives(x, group, key, order=2)
+def s2s_terms(x, group, YY, XX, key='Rnt'):
+	d1_x, d2_x = validate_key_and_derivatives(x, group, YY, XX, key, order=2)
 	lib = {}
 	attrs = {}
 
@@ -50,11 +50,11 @@ def s2s_terms(x, group, key='Rnt'):
 	write_library_to_dataset(lib, group.require_group('scalar_library'), attrs)
 
 
-def s2t_terms(x, group, key='Rnt'):
+def s2t_terms(x, group, YY, XX, key='Rnt'):
 	'''
 	Compute terms mapping scalars to symmetric tensors
 	'''
-	d1_x, d2_x = validate_key_and_derivatives(x, group, key, order=2)
+	d1_x, d2_x = validate_key_and_derivatives(x, group, YY, XX, key, order=2)
 	lib = {}
 	attrs = {}
 	
@@ -78,6 +78,7 @@ def build_scalar_library(folder, embryoID, group, key='c', base='cyt',
 	try:
 		S = project_embryo_data(folder, embryoID, base, threshold)
 	except Exception as e:
+		embryoID = str(embryoID)
 		print('Could not load projected components for', embryoID, e)
 		print('Loading %s2D.npy' % base)
 		S = np.load(os.path.join(folder, embryoID, base+'2D.npy'), mmap_mode='r')
@@ -87,5 +88,8 @@ def build_scalar_library(folder, embryoID, group, key='c', base='cyt',
 		S = np.stack([gaussian_filter(S[i], sigma=sigma) for i in range(S.shape[0])])
 
 	S = S.reshape([S.shape[0], *S.shape[-2:]])
-	s2s_terms(S, group, key=key)
+	embryoID = str(embryoID)
+	dv_coordinates = np.load(os.path.join(folder, embryoID, 'DV_coordinates.npy'), mmap_mode='r')
+	ap_coordinates = np.load(os.path.join(folder, embryoID, 'AP_coordinates.npy'), mmap_mode='r')
+	s2s_terms(S, group, dv_coordinates, ap_coordinates, key=key)
 	#s2t_terms(S, group, key=key)

library_generation/tensor_library.py
@@ -9,8 +9,8 @@ from utils.derivative_library_utils import *
 '''
 SYMMETRIC Tensor libraries for anisotropy fields
 '''
-def t2s_terms(x, group, key='m'):
-	d1_x, d2_x = validate_key_and_derivatives(x, group, key, order=2)
+def t2s_terms(x, group, YY, XX, key='m'):
+	d1_x, d2_x = validate_key_and_derivatives(x, group, YY, XX, key, order=2)
 	lib = {}
 	attrs = {}
 	
@@ -44,11 +44,11 @@ def t2s_terms(x, group, key='m'):
 	
 	write_library_to_dataset(lib, group.require_group('scalar_library'), attrs)
 
-def t2t_terms(x, group, key='m'):
+def t2t_terms(x, group, YY, XX, key='m'):
 	'''
 	Compute terms mapping tensors to symmetric tensors
 	'''
-	d1_x = validate_key_and_derivatives(x, group, key, order=1)[0]
+	d1_x = validate_key_and_derivatives(x, group, YY, XX, key, order=1)[0]
 	
 	lib = {}
 	attrs = {}
@@ -77,7 +77,11 @@ def build_tensor_library(folder, embryoID, group, key='m',base='tensor',
 			print('Skipping ', embryoID, e)
 			return
 	else:
+		embryoID = str(embryoID)
 		T = np.load(os.path.join(folder, embryoID, base+'2D.npy'), mmap_mode='r')
 
+	embryoID = str(embryoID)
 	T = T.reshape([T.shape[0], 2, 2, *T.shape[-2:]])
-	t2t_terms(T, group, key=key)
+	dv_coordinates = np.load(os.path.join(folder, embryoID, 'DV_coordinates.npy'), mmap_mode='r')
+	ap_coordinates = np.load(os.path.join(folder, embryoID, 'AP_coordinates.npy'), mmap_mode='r')
+	t2t_terms(T, group, dv_coordinates, ap_coordinates, key=key)

library_generation/vector_library.py
@@ -11,12 +11,12 @@ from utils.derivative_library_utils import *
 '''
 Generate different library terms from vector fields
 '''
-def v2t_terms(u, group, key='v'):
+def v2t_terms(u, group, YY, XX, key='v'):
 	'''
 	The velocity derivative terms we care about are VORTICITY and STRAIN RATE
 	We will decompose them further and couple them with tensors later in the pipeline
 	'''
-	d1_u = validate_key_and_derivatives(u, group, 'v', 1)[0]
+	d1_u = validate_key_and_derivatives(u, group, YY, XX, key, 1)[0]
 
 	lib = {}
 	attrs = {}
@@ -56,6 +56,10 @@ def build_vector_library(folder, embryoID, group, key='v', base='velocity',
 			print('Skipping ', embryoID, e)
 			return
 	else:
+		embryoID = str(embryoID)
 		V = np.load(os.path.join(folder, embryoID, base+'2D.npy'), mmap_mode='r')
-
-	v2t_terms(V, group)
+	
+	embryoID = str(embryoID)
+	dv_coordinates = np.load(os.path.join(folder, embryoID, 'DV_coordinates.npy'), mmap_mode='r')
+	ap_coordinates = np.load(os.path.join(folder, embryoID, 'AP_coordinates.npy'), mmap_mode='r')
+	v2t_terms(V, group, dv_coordinates, ap_coordinates)

utils/dataset.py
@@ -28,7 +28,7 @@ class ToTensor(object):
 		sample['value'] = torch.tensor(sample['value'].copy(), dtype=torch.float32)
 		return sample
 
-atlas_dir = '/project/vitelli/jonathan/REDO_fruitfly/src/data'
+atlas_dir = '/project/vitelli/jonathan/REDO_fruitfly/src/Public'
 
 class AtlasDataset(torch.utils.data.Dataset):
 	def __init__(self,
@@ -41,10 +41,18 @@ class AtlasDataset(torch.utils.data.Dataset):
 		self.path = os.path.join(atlas_dir, genotype, label)
 		self.filename = filename
 		self.df = pd.read_csv(os.path.join(atlas_dir, genotype, label, 'dynamic_index.csv'))
+
 		if drop_no_time:
 			self.df = self.df.dropna(axis=0)
 		else:
-			self.df.loc[np.isnan(self.df.time), 'time'] = self.df.loc[np.isnan(self.df.time), 'eIdx']
+			#self.df.loc[np.isnan(self.df.time), 'time'] = self.df.loc[np.isnan(self.df.time), 'eIdx']
+			self.df.time = self.df.eIdx
+
+		if os.path.exists(os.path.join(atlas_dir, genotype, label, 'morphodynamic_offsets.csv')):
+			morpho = pd.read_csv(os.path.join(atlas_dir, genotype, label, 'morphodynamic_offsets.csv'), index_col='embryoID')
+			for eId in self.df.embryoID.unique():
+				self.df.loc[self.df.embryoID == eId, 'time'] -= morpho.loc[eId, 'offset']
+
 
 		self.values = {}
 

utils/decomposition_utils.py
@@ -136,7 +136,7 @@ class SVDPipeline(Pipeline):
 				self.steps[i][1].fit(y0)
 			else:
 				self.steps[i][1].fit(X, y0)
-
+			
 			X = self.steps[i][1].transform(X)
 			y0 = self.steps[i][1].transform(y0)
 			
@@ -157,6 +157,11 @@ class SVDPipeline(Pipeline):
 		return Xt
 
 	def inverse_transform(self, Xt, keep=None):
+		if self.whiten:
+			factor = np.sqrt(self['svd'].explained_variance_)
+		else:
+			factor = np.ones_like(self['svd'].explained_variance_)
+
 		if keep is not None:
 			X = np.zeros([Xt.shape[0], self['svd'].n_components])
 			if Xt.shape[-1] == keep.shape[0]:
@@ -164,13 +169,8 @@ class SVDPipeline(Pipeline):
 			else:
 				X[:, keep] = Xt
 			Xt = X
-		
-		if self.whiten:
-			factor = np.sqrt(self['svd'].explained_variance_)
-			return super(SVDPipeline, self).inverse_transform(Xt * factor)
-		else:
-			return super(SVDPipeline, self).inverse_transform(Xt)
-			
+
+		return super(SVDPipeline, self).inverse_transform(Xt * factor)
 
 	def score(self, X, metric=residual, multioutput='raw_values'):
 		y = self.inverse_transform(self.transform(X))
@@ -190,7 +190,7 @@ class SVDPipeline(Pipeline):
 			return score
 		
 from torchvision.transforms import Compose
-def build_decomposition_model(dataset, model_type=SVDPipeline, tmin=0, tmax=60, **model_kwargs):
+def build_decomposition_model(dataset, model_type=SVDPipeline, tmin=-15, tmax=45, **model_kwargs):
 	'''
 	Learn a decomposition model on an AtlasDataset object
 	'''
@@ -227,7 +227,7 @@ def build_decomposition_model(dataset, model_type=SVDPipeline, tmin=0, tmax=60, 
 	if dataset.filename == 'velocity2D':
 		scaler_train = np.zeros([1, *train.shape[-3:]])
 	else:
-		scaler_train = y0[df[(train_mask) & (np.abs(df.time - 5) < 10)].index]
+		scaler_train = y0[df[(train_mask) & (df.time < 0)].index]
 	
 	model.fit(train, scaler_train)
 
@@ -251,6 +251,8 @@ def get_decomposition_results(dataset,
 	base = dataset.filename[:-2]
 	if model_name is None:
 		model_name = model_type.__name__
+	if not os.path.exists(os.path.join(dataset.path, 'decomposition_models')):
+		os.mkdir(os.path.join(dataset.path, 'decomposition_models'))
 	path = os.path.join(dataset.path, 'decomposition_models', '%s_%s' % (base, model_name))
 	if not overwrite and os.path.exists(path+'.pkl'):
 		print('Found SVDPipeline for this dataset!')

utils/derivative_library_utils.py
@@ -37,27 +37,31 @@ def project_embryo_data(folder, embryoID, base, threshold=0.95, model_type=SVDPi
 
 	return model.inverse_transform(params, keep)
 
-def get_derivative_tensors(x, order=2):
+def get_derivative_tensors(x, YY, XX, order=2):
 	'''
 	Return derivative tensors up to specified order
 	Return shape is [..., Y, X, I, J, K, ...]
 		Assumes the last two axes of x are spatial (Y, X)
 		Places the derivative index axes at the end
-	Here we use the coordinates from vitelli_sharing/pixel_coordinates.mat
-	Thus, we compute spatial derivatives in units of PIV scale pixels NOT microns
-	The PIV image size is 0.4 x [original dimensions], so each PIV pixel is 
-		equivalent to 2.5 x original pixel size 
-	The original units are 1pix=0.2619 um, so 1 PIV pix = 0.65479 um
+
+	We now pass coordinates from the folder which are in units of microns
+
+	Deprecated note:
+		Here we use the coordinates from vitelli_sharing/pixel_coordinates.mat
+		Thus, we compute spatial derivatives in units of PIV scale pixels NOT microns
+		The PIV image size is 0.4 x [original dimensions], so each PIV pixel is 
+			equivalent to 2.5 x original pixel size 
+		The original units are 1pix=0.2619 um, so 1 PIV pix = 0.65479 um
 	'''
-	geometry = loadmat('/project/vitelli/jonathan/REDO_fruitfly/flydrive.synology.me/minimalData/vitelli_sharing/pixel_coordinates.mat')
-	XX, YY = geometry['XX'][0, :], geometry['YY'][:, 0]
+	#geometry = loadmat('/project/vitelli/jonathan/REDO_fruitfly/flydrive.synology.me/minimalData/vitelli_sharing/pixel_coordinates.mat')
+	#XX, YY = geometry['XX'][0, :], geometry['YY'][:, 0]
 
 	# Change units of coordinates to microns instead of pixels
-	original_pixel_size = 0.2619 #microns
-	piv_rescale_factor = 0.4
-	piv_pixel_size = original_pixel_size / piv_rescale_factor
-	XX *= piv_pixel_size
-	YY *= piv_pixel_size
+	#original_pixel_size = 0.2619 #microns
+	#piv_rescale_factor = 0.4
+	#piv_pixel_size = original_pixel_size / piv_rescale_factor
+	#XX *= piv_pixel_size
+	#YY *= piv_pixel_size
 
 	diffY = ps.SmoothedFiniteDifference(d=1, axis=-2)
 	diffX = ps.SmoothedFiniteDifference(d=1, axis=-1)
@@ -71,14 +75,14 @@ def get_derivative_tensors(x, order=2):
 		diffY.smoother_kws['axis'] = -(2+i)
 		diffX.smoother_kws['axis'] = -(1+i)
 		xi = np.stack([
-			diffY._differentiate(xi, YY),
-			diffX._differentiate(xi, XX),
+			diffY._differentiate(xi, YY[:, 0]),
+			diffX._differentiate(xi, XX[0, :]),
 		], axis=-1)
 		d.append(xi.copy())
 
 	return d
 
-def validate_key_and_derivatives(x, group, key, order=2):
+def validate_key_and_derivatives(x, group, YY, XX, key, order=2):
 	'''
 	Ensure that key and its derivatives up to specified order are present in the dataset
 	Compute the derivatives if they are not
@@ -97,12 +101,20 @@ def validate_key_and_derivatives(x, group, key, order=2):
 	
 	if flag:
 		print('Computing derivatives!')
-		dx = get_derivative_tensors(x, order=order)
+		dx = get_derivative_tensors(x, YY, XX, order=order)
 		for i in range(order):
 			name = 'D%d %s' % (i+1, key)
 			if name in group:
 				del group[name]
 			group.create_dataset(name, data=dx[i])
+
+		if 'AP_coordinates' in group:
+			del group['AP_coordinates']
+		group.create_dataset('AP_coordinates', data=XX)
+		
+		if 'DV_coordinates' in group:
+			del group['DV_coordinates']
+		group.create_dataset('DV_coordinates', data=YY)
 			
 	return dx
 

utils/translation_utils.py
@@ -223,139 +223,125 @@ def decompose_library(data,
 		ds[:] = dot_cpt
 		ds.attrs.update(X_dot[key].attrs)
 
-def collect_decomposed_data(h5f, key, tmin, tmax, material_derivative=True):
+def collect_overleaf_data(h5f, key, tmin, tmax):
 	'''
-	Collect the PCA data from a	given h5f library and return X, U, X_dot, and the relevant variable names
+	Collect only those terms which correspond to the proposed Overleaf equations
 	'''
+	if key == 'c':
+		feature_names = [
+			'Dorsal_Source', 
+			'c', 
+			'c Tr(E)',
+			#'c Tr(m_ij)',
+			#'c^2', 
+			#'c^2 Tr(m_ij)',
+			'v dot grad c', 
+		]
+	elif key == 'm_ij':
+		feature_names = [
+			'v dot grad m_ij',
+			'[O, m_ij]', 
+			'm_ij',
+			'{m_ij, E_passive}',
+			'c {m_ij, E_passive}',
+			#'Static_DV',
+			'c Static_DV',
+			'Static_DV Tr(m_ij)',
+			'c m_ij',
+			'c m_ij Tr(m_ij)',
+		]
+	else:
+		raise RuntimeError('Overleaf model not proposed for %s' % key)
+
+	return collect_decomposed_data(h5f, key, tmin, tmax, feature_names)
+
+def collect_decomposed_data(h5f, key, tmin, tmax, feature_names=None):
+	'''
+	Collect the PCA data from a given h5f library and return X, X_dot, and the feature names
+	'''
+	if feature_names is None:
+		feature_names = list(h5f['X_cpt'][key].attrs['feature_names'])
+	
 	t_X = h5f['t'][()].astype(int)
 	t_mask = np.logical_and(t_X >= tmin, t_X <= tmax)
-	times = t_X[t_mask]
 
-	feature_names = list(h5f['X_cpt'][key].attrs['feature_names'])
-	X = h5f['X_cpt'][key][t_mask, ...]
+	data = h5f['X_cpt'][key]
+
+	base_names = list(data.attrs['feature_names'])
+	X = []
+	for feature in feature_names:
+		loc = base_names.index(feature)
+		X.append(data[..., loc])
+
+	X = np.stack(X, axis=-1)[t_mask, ...]
 	X_dot = h5f['X_dot_cpt'][key][t_mask, ...]
 
-	if material_derivative:
-		adv = 'v dot grad %s' % key
-		if adv in feature_names:
-			loc = feature_names.index(adv)
-			X_dot += X[..., loc:loc+1]
-			X = np.delete(X, loc, axis=-1)
-			feature_names.remove(adv)
-
-		cor = '[O, %s]' % key
-		if cor in feature_names:
-			loc = feature_names.index(cor)
-			X_dot += X[..., loc:loc+1]
-			X = np.delete(X, loc, axis=-1)
-			feature_names.remove(cor)
-
-	#Eliminate other advection terms because they involve gradients of proteins
-	adv = [feature for feature in feature_names if 'v dot grad' in feature]# or 'E_active' in feature]
-	for a in adv:
-		loc = feature_names.index(a)
-		X = np.delete(X, loc, axis=-1)
-		feature_names.remove(a)
-
-	#If we have control fields, add them in
-	if 'U_cpt' in h5f:
-		t_U = h5f['U_cpt'].attrs['t']
-		control_names = list(h5f['U_cpt'][key].attrs['feature_names'])
-		x_mask = np.logical_and(t_X >= tmin, t_X <= tmax)
-		u_mask = np.logical_and(t_X >= np.min(t_U), t_X <= np.max(t_U))
-		t_mask = np.logical_and(x_mask, u_mask)
-		X = X[t_mask, ...]
-		X_dot = X_dot[t_mask, ...]
-
-		x_mask = np.logical_and(t_U >= np.min(t_X), t_U <= np.max(t_X))
-		u_mask = np.logical_and(t_U >= tmin, t_U <= tmax)
-		t_mask = np.logical_and(x_mask, u_mask)
-		U = h5f['U_cpt'][key][t_mask, ...]
-
-		feature_names = feature_names + control_names
-		X = np.concatenate([X, U], axis=-1)
-	
 	return X, X_dot, feature_names
 
-def fit_sindy_model(h5f, key, tmin, tmax, keep, 
+def repeat_components(x, N):
+	'''
+	Repeat PCA components during fitting procedure to over-weight them
+	'''
+	N[N == 0] = 1 #Avoid eliminating components
+	X = []
+	for i in range(x.shape[1]):
+		X.append(np.repeat(x[:, i:i+1], N[i], axis=1))
+	return np.concatenate(X, axis=1)
+
+def fit_sindy_model(h5f, key, tmin, tmax,
+					component_weight=None,
 					threshold=1e-1, 
 					alpha=1e-1, 
 					n_models=5,
 					n_candidates_to_drop=5,
 					n_subset=None,
-					scale_units=False, 
-					material_derivative=False):
+					material_derivative=False, 
+					component_repeat=None,
+					overleaf_only=False,
+					**kwargs):
 	'''
 	Fit a SINDy model on data filled by a given key in an h5py file
 	Fit range goes from tmin to tmax, and is applied on a set keep of PCA components
 	'''
+	if overleaf_only:
+		print('Using only overleaf-allowed terms')
+		collect_function = collect_overleaf_data
+	else:
+		collect_function = collect_decomposed_data
+
+	#Collect data
+	print('Collecting data')
 	X, X_dot = [], []
-	eIds = list(h5f.keys())
-	for eId in eIds:
-		x, x_dot, feature_names = collect_decomposed_data(h5f[eId], key, tmin, tmax, material_derivative)
+	for eId in list(h5f.keys()):
+		x, x_dot, feature_names = collect_function(h5f[eId], key, tmin, tmax)
 		X.append(x)
 		X_dot.append(x_dot)
-
-	#if len(X) > 1:
-	#	X, _, X_dot, _ = train_test_split(X, X_dot, test_size=0.33)
-
-	X = np.concatenate(X, axis=0)[:, keep]
-	X_dot = np.concatenate(X_dot, axis=0)[:, keep]
-
-	optimizer = ps.STLSQ(threshold=threshold, alpha=alpha, normalize_columns=scale_units)
-	if n_models > 1:
-		bagging = True
-		if n_candidates_to_drop == 0:
-			n_candidates_to_drop = None
-		if n_subset is None:
-			n_subset = X.shape[0]
-
-		n_candidates_to_drop = None if n_candidates_to_drop == 0 else n_candidates_to_drop
-		optimizer = ps.EnsembleOptimizer(
-			opt=optimizer,
-			bagging=True,
-			library_ensemble=n_candidates_to_drop is not None,
-			n_models=n_models,
-			n_subset=n_subset,
-			n_candidates_to_drop=n_candidates_to_drop)
-
-	sindy = ps.SINDy(
-		feature_library=ps.IdentityLibrary(),
-		optimizer=optimizer,
-		feature_names=feature_names,
-	)
-
-	sindy.fit(x=X, x_dot=X_dot)
-	sindy.material_derivative_ = material_derivative
-	sindy.print(lhs=['D_t %s ' % key])
-	return sindy
-
-def fit_new_sindy_model(h5f, key, tmin, tmax,
-					    component_weight=None,
-					    threshold=1e-1, 
-					    alpha=1e-1, 
-					    n_models=5,
-					    n_candidates_to_drop=5,
-					    n_subset=None,
-					    material_derivative=False, 
-						**kwargs):
-	'''
-	Fit a SINDy model on data filled by a given key in an h5py file
-	Fit range goes from tmin to tmax, and is applied on a set keep of PCA components
-	'''
-	X, X_dot = [], []
-	eIds = list(h5f.keys())
-	for eId in eIds:
-		x, x_dot, feature_names = collect_decomposed_data(h5f[eId], key, tmin, tmax, material_derivative)
-		X.append(x)
-		X_dot.append(x_dot)
-
-	#if len(X) > 1:
-	#	X, _, X_dot, _ = train_test_split(X, X_dot, test_size=0.33)
-
 	X = np.concatenate(X, axis=0)
 	X_dot = np.concatenate(X_dot, axis=0)
 
+	#Shift material derivative terms to LHS
+	if material_derivative:
+		print('Adding Material Derivative terms to LHS')
+		for feat in ['v dot grad %s' % key, '[O, %s]' % key]:
+			if feat in feature_names:
+				loc = feature_names.index(feat)
+				X_dot += X[..., loc:loc+1]
+				X = np.delete(X, loc, axis=-1)
+				feature_names.remove(feat)
+	
+	#HARDCODED overweighting of component 0
+	if component_repeat is not None:
+		print('Repeating components by this factor %s' % str(component_repeat))
+		assert component_repeat.shape[0] == X.shape[1]
+		X = repeat_components(X, component_repeat)
+		X_dot = repeat_components(X_dot, component_repeat)
+		component_weight = repeat_components(component_weight[None], component_repeat)[0]
+
+	if len(X) > 1:
+		print('Applying train/test split')
+		X, _, X_dot, _ = train_test_split(X, X_dot, test_size=0.33)
+
+	#Build optimizer (with ensembling)
 	optimizer = ps.STLSQ(threshold=threshold, alpha=alpha, normalize_columns=True)
 	if n_models > 1:
 		bagging = True
@@ -377,116 +363,12 @@ def fit_new_sindy_model(h5f, key, tmin, tmax,
 		feature_names=feature_names,
 	)
 	
-	#HARDCODED overweighting of component 0
-	'''
-	N = 5
-	X = np.concatenate([
-		np.repeat(X[:, 0:1], N, axis=1),
-		X[:, 1:]], axis=1)
-	X_dot = np.concatenate([
-		np.repeat(X_dot[:, 0:1], N, axis=1),
-		X_dot[:, 1:]], axis=1)
-	component_weight = np.concatenate([
-		np.repeat(component_weight[0:1], N),
-		component_weight[1:]])
-	print(X.shape, component_weight.shape)
-	'''
-
+	#Fit and print
 	sindy.fit(x=X, x_dot=X_dot, component_weight=component_weight)
 	sindy.material_derivative_ = material_derivative
 	sindy.print(lhs=['D_t %s ' % key])
 	return sindy
 
-def fit_overleaf_model(h5f, key, tmin, tmax, keep, threshold=0, alpha=1e-1, n_models=5, n_subset=30, **kwargs):
-	X, X_dot = [], []
-	eIds = list(h5f.keys())
-	if key == 'c':
-		feature_names = [
-			'Dorsal_Source', 
-			'c', 
-			'c Tr(E)',
-			#'c Tr(m_ij)',
-			#'c^2', 
-			#'c^2 Tr(m_ij)',
-			'v dot grad c', 
-		]
-	if key == 'm_ij':
-		feature_names = [
-			'v dot grad m_ij',
-			'[O, m_ij]', 
-			'm_ij',
-			'{m_ij, E_passive}',
-			'c {m_ij, E_passive}',
-			#'Static_DV',
-			'c Static_DV',
-			'Static_DV Tr(m_ij)',
-			'c m_ij',
-			'c m_ij Tr(m_ij)',
-		]
-
-
-	for eId in eIds:
-		data = h5f[eId]
-		t_X = data['t'][()].astype(int)
-		t_mask = np.logical_and(t_X >= tmin, t_X <= tmax)
-
-		x_eId = data['X_cpt'][key]
-		base_names = list(x_eId.attrs['feature_names'])
-		x = []
-
-		for i, fn in enumerate(feature_names):
-			loc = base_names.index(fn)
-			x.append(x_eId[..., loc][()])
-		x = np.stack(x, axis=-1)
-		
-		X.append(x[t_mask, ...])
-		X_dot.append(data['X_dot_cpt'][key][t_mask, ...])
-	
-	if len(X) > 1:
-		X, _, X_dot, _ = train_test_split(X, X_dot, test_size=0.33)
-
-	X = np.concatenate(X, axis=0)[:, keep]
-	X_dot = np.concatenate(X_dot, axis=0)[:, keep]
-
-	#HARDCODED overweighting of component 0
-	N = 2
-	X = np.concatenate([
-		np.repeat(X[:, 0:1], N, axis=1),
-		X[:, 1:]], axis=1)
-	X_dot = np.concatenate([
-		np.repeat(X_dot[:, 0:1], N, axis=1),
-		X_dot[:, 1:]], axis=1)
-
-	#Material derivative terms
-	for feat in ['v dot grad %s' % key, '[O, %s]' % key]:
-		if feat in feature_names:
-			loc = feature_names.index(feat)
-			X_dot += X[..., loc:loc+1]
-			X = np.delete(X, loc, axis=-1)
-			feature_names.remove(feat)
-	
-	optimizer = ps.STLSQ(threshold=threshold, alpha=alpha, normalize_columns=True)
-	if n_models > 1:
-		optimizer = ps.EnsembleOptimizer(
-			opt=optimizer,
-			bagging=True,
-			library_ensemble=False,
-			n_models=n_models,
-			n_subset=n_subset,
-			n_candidates_to_drop=None)
-	
-	sindy = ps.SINDy(
-		feature_library=ps.IdentityLibrary(),
-		optimizer=optimizer,
-		feature_names=feature_names,
-	)
-
-
-	sindy.fit(x=X, x_dot=X_dot)
-	sindy.material_derivative_ = True
-	sindy.print(lhs=['D_t %s ' % key])
-	return sindy
-	
 	
 def evolve_rk4_grid(x0, x_dot, model, keep, tmin=0, tmax=10, step_size=0.2):
 	'''
@@ -655,17 +537,16 @@ def decomposed_predictions_plot(x_pred, x_int, x_model, times, keep):
 	nrows = ceil(cpt_pred.shape[1] / ncols)
 	fig, ax = plt.subplots(nrows, ncols, 
 						   figsize=(ncols, nrows),
-						   sharey=True, sharex=True, dpi=200)
+						   sharey=False, sharex=True, dpi=200)
 	ax = ax.flatten()
 
 	for i in range(cpt_pred.shape[1]):
 		ax[i].plot(times, cpt_pred[:, i], color='red')
 		ax[i].plot(times, cpt_true[:, i], color='black')
-		ax[i].text(0.98, 0.02, 'Component %d' % i, 
-				   fontsize=6, color='blue',
-				   transform=ax[i].transAxes, va='bottom', ha='right')
-		ax[i].text(0.98, 0.98, 'R2=%g' % r2_score(cpt_true[:, i], cpt_pred[:, i]),
-				   fontsize=6, color='blue',
-				   transform=ax[i].transAxes, va='top', ha='right')
-		ax[i].tick_params(which='both', labelsize=6)
+		ax[i].set_ylabel('Component %d' % i, fontsize=6)
+		ax[i].text(0.02, 0.98, 'R2=%.3g' % r2_score(cpt_true[:, i], cpt_pred[:, i]),
+				   fontsize=5, color='blue',
+				   transform=ax[i].transAxes, va='top', ha='left')
+		ax[i].tick_params(which='both', labelsize=4)
+		ax[i].set_yticks([])
 	plt.tight_layout()