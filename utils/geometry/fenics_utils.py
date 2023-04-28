Added some new gradient methods which are arguably more correct but not necessarily better

utils/geometry/fenics_utils.py
@@ -54,7 +54,10 @@ class FenicsGradient(BaseEstimator, TransformerMixin, Module):
 		self.ops = {}
 		mesh = Mesh(os.path.join(geo_dir, f'{self.mesh_name}.xml'))
 		if tangent is None:
-			tangent = TangentSpaceTransformer(mesh_name=self.mesh_name).fit(X)
+			if self.mode == 'torch':
+				tangent = TangentSpaceTransformer(mesh_name=self.mesh_name).fit(X.cpu().numpy())
+			else:
+				tangent = TangentSpaceTransformer(mesh_name=self.mesh_name).fit(X)
 
 		print('Building mesh gradient operators')
 		for order in range(3):
@@ -233,61 +236,147 @@ class FenicsGradient_v2(FenicsGradient):
 	'''
 
 	def build_gradient_operators(self, order, mesh, tangent):
-		self.tangent = tangent
-
-		if order == 2:
-			fe = TensorElement('CG', mesh.ufl_cell(), 1)
-		elif order == 1:
-			fe = VectorElement('CG', mesh.ufl_cell(), 1)
+		#Check if it exists in file
+		fname = os.path.join(geo_dir, 'gradient_operators', f'{self.mesh_name}_full_order_{order}.npz')
+		if os.path.exists(fname):
+			print(f'Loading order {order} from file')
+			L = sparse.load_npz(fname)
 		else:
-			fe = FiniteElement('CG', mesh.ufl_cell(), 1)
+			if order == 2:
+				fe1 = TensorElement('CG', mesh.ufl_cell(), 1)
+				fe2 = TensorElement('CG', mesh.ufl_cell(), 1, shape=(3,3,3,))
+			elif order == 1:
+				fe1 = VectorElement('CG', mesh.ufl_cell(), 1)
+				fe2 = TensorElement('CG', mesh.ufl_cell(), 1)
+			else:
+				fe1 = FiniteElement('CG', mesh.ufl_cell(), 1)
+				fe2 = VectorElement('CG', mesh.ufl_cell(), 1)
 
-		FS = FunctionSpace(mesh, fe)
-		num_vertices = mesh.coordinates().shape[0]
-		v2d = vertex_to_dof_map(FS).reshape([num_vertices, -1]).T.flatten()
+			FS1 = FunctionSpace(mesh, fe1)
+			FS2 = FunctionSpace(mesh, fe2)
+			num_vertices = mesh.coordinates().shape[0]
+			v2d1 = vertex_to_dof_map(FS1).reshape([num_vertices, -1]).T.flatten()
+			v2d2 = vertex_to_dof_map(FS2).reshape([num_vertices, -1]).T.flatten()
 		
-		N = tangent.N[order]
+			A = assemble( inner(TrialFunction(FS2), TestFunction(FS2)) * dx)
+			A = np.array(A.array())
+			A = A @ np.ones(A.shape[1])
+			A = 1. / A
 
-		u = TrialFunction(FS)
-		v = TestFunction(FS)
-		A = build_operator( inner(u, v) * dx, v2d)
-		A = N @ A @ N.T
+			L = assemble( inner(grad(TrialFunction(FS1)), TestFunction(FS2)) * dx)
+			L = np.array(L.array())
 
-		grad = []
-		grad.append(A)
+			L = L * A[:, None]
+			L = sparse.csr_matrix(L)
 
-		for i in range(3):
-			A = build_operator( -inner(u, v.dx(i)) * dx, v2d)
-			A = N @ A @ N.T
-			grad.append(A)
-		
-		#Make sure we have the tangent space matrix for this
-		if not order+1 in tangent.N:
-			tangent.build_N_matrix(order+1)
-		N_out = tangent.N[order+1]
+			#Reorder degrees of freedom
+			L = L[v2d2, :]
+			L = L[:, v2d1]
 
-		return grad
+			#Make sure we have the tangent space matrix for this
+			if not order in tangent.N:
+				tangent.build_N_matrix(order)
+			if not order+1 in tangent.N:
+				tangent.build_N_matrix(order+1)
+			N1 = tangent.N[order]
+			N2 = tangent.N[order+1]
+
+			L = N2 @ L @ N1.T #Push operator to tangent space
+			
+			sparse.save_npz(fname, L)
+
+		if self.mode == 'torch':
+			L = torch.from_numpy(L.todense()).to_sparse()
+
+		return L
 	
 	def transform(self, X):
 		n_components = np.prod(X.shape[:-1])
 		order = int(np.log2(n_components))
 
-		ops = self.ops[order]
-		grad = []
-
-		for i in range(1, len(ops)):
-			d = ops[i] @ X.flatten()
-			d = sparse.linalg.spsolve(ops[0], d)
-			d = self.tangent.N[order].T @ d #Push to 3D space
-			grad.append(d)
-
-		grad = np.stack(grad)
-		grad = self.tangent.N[order+1] @ grad.flatten() #Push to tangent space
+		grad = self.ops[order] @ X.flatten()
 		grad = grad.reshape([2, *X.shape])
 
+		#Note that higher order function space places gradient at last index
 		if self.mode == 'numpy':
-			grad = np.moveaxis(grad, 0, -1)
+			grad = np.moveaxis(grad, -2, -1)
 		else:
-			grad = torch.moveaxis(grad, 0, -1)
+			grad = torch.moveaxis(grad, -2, -1)
+
+		return grad / self.pixel_scale
+
+class FenicsGradient_v3(FenicsGradient):
+	'''
+	This is an attempt using sparse spsolve instead of matrix operations
+	It turns out that preserving sparsity does not make things significantly faster
+	'''
+
+	def build_gradient_operators(self, order, mesh, tangent):
+		#Check if it exists in file
+		fname = os.path.join(geo_dir, 'gradient_operators', f'{self.mesh_name}_full_order_{order}.npy')
+		if os.path.exists(fname):
+			print(f'Loading order {order} from file')
+			L = np.load(fname, mmap_mode='r')
+		else:
+			if order == 2:
+				fe1 = TensorElement('CG', mesh.ufl_cell(), 1)
+				fe2 = TensorElement('CG', mesh.ufl_cell(), 1, shape=(3,3,3,))
+			elif order == 1:
+				fe1 = VectorElement('CG', mesh.ufl_cell(), 1)
+				fe2 = TensorElement('CG', mesh.ufl_cell(), 1)
+			else:
+				fe1 = FiniteElement('CG', mesh.ufl_cell(), 1)
+				fe2 = VectorElement('CG', mesh.ufl_cell(), 1)
+
+			FS1 = FunctionSpace(mesh, fe1)
+			FS2 = FunctionSpace(mesh, fe2)
+			num_vertices = mesh.coordinates().shape[0]
+			v2d1 = vertex_to_dof_map(FS1).reshape([num_vertices, -1]).T.flatten()
+			v2d2 = vertex_to_dof_map(FS2).reshape([num_vertices, -1]).T.flatten()
+		
+			A = assemble( inner(TrialFunction(FS2), TestFunction(FS2)) * dx)
+			A = sparse.csr_matrix(np.array(A.array()))
+
+			L = assemble( inner(grad(TrialFunction(FS1)), TestFunction(FS2)) * dx)
+			L = sparse.csr_matrix(np.array(L.array()))
+
+
+			L = sparse.linalg.spsolve(A, L)
+
+			#Reorder degrees of freedom
+			L = L[v2d2, :]
+			L = L[:, v2d1]
+
+			#Make sure we have the tangent space matrix for this
+			if not order in tangent.N:
+				tangent.build_N_matrix(order)
+			if not order+1 in tangent.N:
+				tangent.build_N_matrix(order+1)
+			N1 = tangent.N[order]
+			N2 = tangent.N[order+1]
+
+			L = N2 @ L @ N1.T #Push operator to tangent space
+			
+			L = np.asarray(L.todense())
+			
+			np.save(fname, L)
+
+		if self.mode == 'torch':
+			L = torch.from_numpy(L)
+
+		return L
+	
+	def transform(self, X):
+		n_components = np.prod(X.shape[:-1])
+		order = int(np.log2(n_components))
+
+		grad = self.ops[order] @ X.flatten()
+		grad = grad.reshape([2, *X.shape])
+		
+		#Note that higher order function space places gradient at last index
+		if self.mode == 'numpy':
+			grad = np.moveaxis(grad, -2, -1)
+		else:
+			grad = torch.moveaxis(grad, -2, -1)
 
 		return grad / self.pixel_scale

utils/geometry/hybrid_loop.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from ..forecasting.closed_loop import ClosedFlyLoop
 from .geometry_utils import TangentSpaceTransformer, MeshInterpolator
-from .fenics_utils import FenicsGradient, FenicsGradient_v0, FenicsGradient_v1
+from .fenics_utils import *
 
 
 class HybridClosedLoop(ClosedFlyLoop):
@@ -20,8 +20,8 @@ class HybridClosedLoop(ClosedFlyLoop):
 		super().fit(X, y0)
 
 		self.tangent   = TangentSpaceTransformer(self.mesh_name).fit(X)
-		self.interp    = MeshInterpolator(self.mesh_name).fit(X)
-		self.gradient  = FenicsGradient_v1(self.mesh_name).fit(X)
+		self.interp    = MeshInterpolator(self.mesh_name, cutoff=0.15).fit(X)
+		self.gradient  = FenicsGradient_v3(self.mesh_name).fit(X)
 
 		del self.gamma_dv_
 		self.gamma_dv_ = np.array([
@@ -97,3 +97,18 @@ class HybridClosedLoop(ClosedFlyLoop):
 		f = self.smoother.transform(f)
 
 		return f
+	
+	def rhs(self, m, s, v, E):
+		'''
+		Compute the right hand side of the myosin dynamics
+		'''
+		trm = self.einsum_('kkyx->yx', m)
+		trE = self.einsum_('kkyx->yx', E)
+
+		rhs  = -(0.066 - 0.061 * s) * m #Detachment
+		rhs +=  (0.489 + 0.318 * s) * m * trE #Strain recruitment
+		#rhs +=  2*(0.489 - 0.118 * s) * m * trE #Strain recruitment
+		rhs +=  (0.564 - 0.393 * s) * trm * m #Tension recruitment
+		rhs +=  (0.047 - 0.037 * s) * trm * self.gamma_dv_ #Hoop stress recruitment
+
+		return rhs