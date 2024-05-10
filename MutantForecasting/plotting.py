968ea6c3691606663079b356c4b5045b09567f58
Jonathan Colen <jcolen@uchicago.edu>
2024-05-10 12:31:52 -0500

    Evaluation on WT and Twist, Toll, Spz

--- MutantForecasting/plotting.py
+++ MutantForecasting/plotting.py
@@ -68,6 +68,8 @@ def sqh_vel_plot(t,
         mnorm = np.linalg.norm(mnorm, axis=1)[..., :-20] #Ignore posterior pole
         mmin = np.min(mnorm)
         mmax = np.max(mnorm)
+        
+        print(mmin, mmax)
     
     N = int(np.ceil(len(t) / dt))
     fig, ax = plt.subplots(2, N+1, 
@@ -109,8 +111,9 @@ def sqh_vel_plot(t,
     ax[1,-1].plot(t, v0norm, color='black', linestyle='--')
     ax[1,-1].plot(t, vnorm,  color='black', linestyle='-')
     
-    ax[0,-1].set(xlim=[dv_min+vfc, dv_max-vfc])
-    ax[0,-1].set(xticks=[dv_min+vfc, dv_midpoint, dv_max-vfc], xticklabels=['D', 'V', 'D'])
+    ax[0,-1].set(xlim=[dv_min+vfc, dv_midpoint])
+    ax[0,-1].set(xticks=[dv_min+vfc, dv_midpoint], xticklabels=['D', 'V'])
+    #ax[0,-1].set(xticks=[dv_min+vfc, dv_midpoint, dv_max-vfc], xticklabels=['D', 'V', 'D'])
     ax[0,-1].set(ylim=[0, 0.25], yticks=[0, 0.1, 0.2])
     ax[0,-1].set_ylabel('Intensity')
     
@@ -126,6 +129,7 @@ def sqh_vel_plot(t,
                  cax=cax, label='Time (min)', ticks=[])
     plt.tight_layout()
 
+def residual_plot(t, m, m0, v, v0):
     fig1, ax = plt.subplots(1, 1, figsize=(1, 1))
     colors = ['firebrick', 'black']
     ax.plot(t, residual(m0, m).mean(axis=(1,2)), color='firebrick', label='Myosin')
@@ -139,155 +143,78 @@ def sqh_vel_plot(t,
     ax.set_xticks([-10, 0, 10, 20])
     ax.legend(fontsize=6)
 
-    return fig, fig1
+from scipy.interpolate import RectBivariateSpline as interp2d
+from math import floor, ceil
+def get_velocity(v, tt, X0, Y0, x, y):
+    idxA, idxB = floor(tt), ceil(tt)
+    frac = 1 - (tt - idxA)
+    vA, vB = v[idxA], v[idxB]
+        
+    vx = interp2d(Y0, X0, frac * vA[1] + (1 - frac) * vB[1])(y, x, grid=False)
+    vy = interp2d(Y0, X0, frac * vA[0] + (1 - frac) * vB[0])(y, x, grid=False)
+    return vx, vy
 
+def rk4DynamicVelocity2D(pts, X0v, Y0v, v, hh):
+    tt = np.arange(0, v.shape[0]-1, hh)
+    xyPathsMat = np.zeros([pts.shape[0], len(tt), 2])
 
-def comparison_plot(t, *fields,
-                    dt=5,
-                    mask=np.s_[..., 20:-20, 0:-25],
-                    al=0.7, labelpad=16):
+    x = pts[:, 0]
+    y = pts[:, 1]
+    xyPathsMat[:, 0, 0] = x
+    xyPathsMat[:, 0, 1] = y
+    
+    Xmin, Xmax = X0v[0], X0v[-1]
+    Ymin, Ymax = Y0v[0], Y0v[-1]
+    
+    for ii in range(len(tt) - 1):
+        k1x, k1y = get_velocity(v, tt[ii], X0v, Y0v, x, y)
+        k2x, k2y = get_velocity(v, tt[ii] + 0.5 * hh, X0v, Y0v, x + 0.5 * hh * k1x, y + 0.5 * hh * k1y)
+        k3x, k3y = get_velocity(v, tt[ii] + 0.5 * hh, X0v, Y0v, x + 0.5 * hh * k2x, y + 0.5 * hh * k2y)
+        k4x, k4y = get_velocity(v, tt[ii] + hh, X0v, Y0v, x + hh * k3x, y + hh * k3y)
+        
+        #Main equation
+        x = x + (k1x + 2 * k2x + 2 * k3x + k4x) * hh / 6.
+        y = y + (k1y + 2 * k2y + 2 * k3y + k4y) * hh / 6.
+        
+        x[x > Xmax] = Xmax
+        x[x < Xmin] = Xmin
+        y[y > Ymax] = Ymax
+        y[y < Ymin] = Ymin
+        
+        xyPathsMat[:, ii, 0] = x
+        xyPathsMat[:, ii, 1] = y
+            
+    return xyPathsMat
+
+def trajectory_plot(v0, v1, nDV=8, nAP=8, hh=0.2):
     '''
-    Plot a comparison plot of multiple fields
-    NOTE: assumes the last field is the velocity field and won't plot DV cuts
-    Instead, it'll do a residual plot in the last row last column
+    Plot trajectories of cells in test flow field
     '''
-    N = int(np.ceil(len(t) / dt))
-    n_rows = len(fields)
-    fig, ax = plt.subplots(n_rows, N+1, 
-                           figsize=(N+1, n_rows*1.2),
-                           constrained_layout=True)
+    DV = np.linspace(dv_min + 25, dv_max - 25, nDV)
+    AP = np.linspace(ap_min + 25, ap_max - 25, nAP)
+    pts = np.stack(np.meshgrid(AP, DV, indexing='xy'), axis=-1).reshape([-1, 2])
 
-    vfc = 40
-    skip = 20
-    ylim = [dv_min, dv_max]
-    xlim = [ap_min, ap_max]
-
-    colors = ['Reds', 'Greys']
-
-    res = []
-
-    for i, (field, z, z0) in enumerate(fields):
-        znorm = z0.reshape([z0.shape[0], -1, *z0.shape[-2:]])
-        n_channels = znorm.shape[1]
-        znorm = np.linalg.norm(znorm[mask], axis=1)[..., :-20] #ignore posterior pole
-        kwargs = dict(vmin=np.min(znorm), vmax=np.max(znorm))
-
-        alpha = np.zeros(z.shape[-2:])
-        alpha0 = np.zeros_like(alpha)
-        midpoint = alpha.shape[0] // 2
-        alpha[midpoint:] = 1
-        alpha0[:midpoint] = al
-
-        dv = np.linspace(dv_min, dv_midpoint, midpoint)
-
-        slc = np.s_[..., ::skip, ::skip]
-        X = np.linspace(ap_min, ap_max, z.shape[-1])
-        Y = np.linspace(dv_min, dv_max, z.shape[-2])
-        Y, X = np.meshgrid(Y, X, indexing='ij')
-
-        top = np.s_[..., midpoint:, :]
-        bot = np.s_[..., :midpoint, :]
-        vwargs = dict(pivot='middle', width=0.005)
-
-        axes = []
-        cmap = plt.get_cmap(colors[i])
-        for j in range(N):
-            jj = dt * j
-            if i == 0:
-                ax[i, j].set_title(f't = {t[jj]:.0f}')
-
-            color = cmap((t[jj] + 20) / (t.max() + 20))	
-
-            if n_channels == 1:
-                color_2D(ax[i, j], z[jj, ::-1], alpha=alpha, **kwargs)
-                color_2D(ax[i, j], z0[jj], alpha=alpha0, **kwargs)
-            elif n_channels == 2:
-                #vwargs['scale'] = 5e1 if t[jj] > 5 else 2e1
-                vwargs['scale'] = 5e1
-                ax[i, j].quiver(X[bot][slc], Y[bot][slc],
-                                z0[jj, 1][bot][slc], z0[jj, 0][bot][slc],
-                                color='grey', **vwargs)
-                ax[i, j].quiver(X[top][slc], Y[top][slc],
-                                z[jj, 1, ::-1][top][slc], -z[jj, 0, ::-1][top][slc],
-                                color='black', **vwargs)
-                ax[i, j].set(xticks=[], yticks=[])
-            elif n_channels == 4:
-                kwargs['both'] = t[jj] < 0
-                zj = z[jj, :, :, ::-1].copy()
-                zj[0, 1] *= -1
-                zj[1, 0] *= -1
-                plot_tensor2D(ax[i, j], zj * alpha, alpha=alpha, **kwargs)
-                plot_tensor2D(ax[i, j], z0[jj] * np.sign(alpha0), alpha=alpha0, **kwargs)
-
-            ax[i, j].set(xlim=xlim, ylim=ylim)
-
-            if i == n_rows - 1:
-                continue
-
-
-            cut = get_cut(z[jj, ..., ::-1, :])
-            cut0 = get_cut(z0[jj])
-            ax[i, -1].plot(dv, cut[::-1][:midpoint], color=color)
-            ax[i, -1].plot(dv, cut0[:midpoint], color=color, linestyle='--')
-
-            cut = cut[midpoint:]
-            cut0 = cut0[:midpoint]
-            ax_Y = ax[i, j].inset_axes([-0.25, 0, 0.2, 1], sharey=ax[i, j])
-            ax_Y.plot(cut, dv+dv_midpoint, color=color, lw=0.5)
-            ax_Y.fill_betweenx(dv+dv_midpoint, 0, cut, color=color, alpha=0.7)
-            ax_Y.plot(cut0, dv, color=color, lw=0.5, alpha=al)
-            ax_Y.fill_betweenx(dv, 0, cut0, color=color, alpha=0.7*al)
-            axes.append(ax_Y)
-
-        ax[i, 0].set_ylabel(field, labelpad=labelpad)
-        #res.append(residual(z0[mask], z[mask]).mean(axis=(1, 2)))
-        if i > 0:
-            res.append(residual(z0[mask], z[mask]).mean(axis=(1, 2)))
-        else:
-            res.append(mean_norm_residual(z0[mask], z[mask]).mean(axis=(1, 2)))
-
-        if n_channels == 2:
-            znorm = znorm.mean(axis=(1, 2))
-            z0norm = np.linalg.norm(
-                z0.reshape([z0.shape[0], -1, *z0.shape[-2:]])[..., :-20], axis=1)
-            znorm = np.linalg.norm(
-                z.reshape([z.shape[0], -1, *z.shape[-2:]])[..., :-20], axis=1)
-            ax[i, -1].plot(t, z0norm.mean(axis=(-1, -2)), color='black', linestyle='--')
-            ax2 = ax[i, -1]
-            ax2.plot(t, znorm.mean(axis=(-1, -2)), color='black')
-            ax2.set_yticks([])
-            ax2.set_yticklabels([])
-            ax[i, -1].set_xlabel('Time (min)')
-            ax[i, -1].set_xticks([-10, 0, 10, 20])
-            ax[i, -1].set_ylabel('Mean flow\n($\\mu$m / min)', labelpad=0)
-        else:
-            ax[i, -1].set_xlim([dv_min+vfc, dv_midpoint])
-            ax[i, -1].set(xticks=[], yticks=[], xlabel='DV')
-            ax[i, -1].autoscale(axis='y')
-            for axis in axes:
-                axis.set(xlim=ax[i, -1].get_ylim())
-                axis.invert_xaxis()
-                axis.axis('off')
-
-    norm = Normalize(vmin=-20, vmax=np.max(t))
-    fig.subplots_adjust(right=0.97, wspace=0.35, hspace=0.3)
-    for i in range(n_rows-1):
-        pos = ax[i, 0].get_position()
-        cax = fig.add_axes([0.98, pos.y0, 0.01, pos.y1-pos.y0])
-        plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=plt.get_cmap(colors[i])),
-                     cax=cax, label='Time (min)', ticks=[])
-
-    fig1, ax = plt.subplots(1, 1, figsize=(1, 1))
-    colors = ['firebrick', 'black']
-    for i in range(2):
-        ax.plot(t, res[i], color=colors[i], label=fields[i][0])
-    ax.axhline(0.25, zorder=0, color='grey', linestyle='--')
-    ax.text(1.01, 0.25, '25%', transform=ax.transAxes,
-            color='grey', va='center', ha='left')
-    ax.set_ylabel('Error Rate')
-    ax.set_ylim([-0.05, 1.05])
-    ax.set_xlabel('Time')
-    ax.set_xticks([-10, 0, 10, 20])
-    ax.legend(fontsize=6)
-
-    return fig, fig1
+    DV = np.linspace(dv_min, dv_max, 236)
+    AP = np.linspace(ap_min, ap_max, 200)
+    
+    y0Paths = rk4DynamicVelocity2D(pts, AP, DV, v0, hh)
+    y1Paths = rk4DynamicVelocity2D(pts, AP, DV, v1, hh)
+    
+    fig, ax = plt.subplots(1, 1, dpi=200)
+    nPts = y0Paths.shape[0]
+    nTps = y0Paths.shape[1]
+    for tpId in range(nTps - 1):
+        ax.scatter(y0Paths[:, tpId, 0], y0Paths[:, tpId, 1], s=3,
+                   c=np.ones(nPts) * tpId,
+                   cmap='Blues', vmin=-10, vmax=nTps*1.2)
+        ax.scatter(y1Paths[:, tpId, 0], y1Paths[:, tpId, 1], s=3,
+                   c=np.ones(nPts) * tpId,
+                   cmap='Reds', vmin=-10, vmax=nTps*1.2)
+    
+    ax.set(xlim=[ap_min, ap_max], xticks=[ap_min, ap_max], xticklabels=['A', 'P'])
+    ax.set(ylim=[dv_min, dv_max], yticks=[dv_min, dv_midpoint, dv_max], yticklabels=['D', 'V', 'D'])
+    ax.set_aspect('equal')
+    
+    ax.text(0.2, 1.02, 'Experiment', color='Blue', va='bottom', ha='center', transform=ax.transAxes)
+    ax.text(0.8, 1.02, 'SINDy', color='Red', va='bottom', ha='center', transform=ax.transAxes)
+    
--- MutantForecasting/train_myosin_translation.py
+++ MutantForecasting/train_myosin_translation.py
@@ -17,191 +17,191 @@ from argparse import ArgumentParser
 Instantaneous translation model
 '''
 class MaskedVAE(VAE):
-    def __init__(self, 
-                 *args,
-                 dv_min=15,
-                 dv_max=-15,
-                 ap_min=15, 
-                 ap_max=-15,
-                 **kwargs):
-        super().__init__(*args, **kwargs)
+	def __init__(self, 
+				 *args,
+				 dv_min=15,
+				 dv_max=-15,
+				 ap_min=15, 
+				 ap_max=-15,
+				 **kwargs):
+		super().__init__(*args, **kwargs)
 
-        # Remove data wherever mask is True
-        self.mask = torch.ones(self.input_size, dtype=bool)
-        self.mask[dv_min:dv_max, ap_min:ap_max] = 0
-        self.mask = torch.nn.Parameter(self.mask, requires_grad=False)
-    
-    def forward(self, x):
-        x[..., self.mask] = 0. # Mask regions marked
-        return super().forward(x)
+		# Remove data wherever mask is True
+		self.mask = torch.ones(self.input_size, dtype=bool)
+		self.mask[dv_min:dv_max, ap_min:ap_max] = 0
+		self.mask = torch.nn.Parameter(self.mask, requires_grad=False)
+	
+	def forward(self, x):
+		x[..., self.mask] = 0. # Mask regions marked
+		return super().forward(x)
 
 def residual(u, v):
-    '''
-    We assume u is the INPUT and v is the TARGET
-    Using residual metric from Sebastian's eLife paper
+	'''
+	We assume u is the INPUT and v is the TARGET
+	Using residual metric from Sebastian's eLife paper
 
-    '''
-    umag = torch.linalg.norm(u, dim=-3)
-    vmag = torch.linalg.norm(v, dim=-3)
+	'''
+	umag = torch.linalg.norm(u, dim=-3)
+	vmag = torch.linalg.norm(v, dim=-3)
 
-    uavg = torch.sqrt(umag.pow(2).mean(dim=(-2, -1), keepdims=True))
-    vavg = torch.sqrt(vmag.pow(2).mean(dim=(-2, -1), keepdims=True))
+	uavg = torch.sqrt(umag.pow(2).mean(dim=(-2, -1), keepdims=True))
+	vavg = torch.sqrt(vmag.pow(2).mean(dim=(-2, -1), keepdims=True))
 
-    res = uavg**2 * vmag**2 + vavg**2 * umag**2 - 2 * uavg * vavg * torch.einsum('...ijk,...ijk->...jk', u, v)
-    denom = 2 * vavg**2 * uavg**2
-    denom[denom == 0] += 1
-    res /= denom
+	res = uavg**2 * vmag**2 + vavg**2 * umag**2 - 2 * uavg * vavg * torch.einsum('...ijk,...ijk->...jk', u, v)
+	denom = 2 * vavg**2 * uavg**2
+	denom[denom == 0] += 1
+	res /= denom
 
-    return res.mean(dim=(-2, -1)) # Average over space
+	return res.mean(dim=(-2, -1)) # Average over space
 
 def mean_squared_error(u, v):
-    sse = (u - v).pow(2).sum(dim=-3)
-    return sse.mean(dim=(-2, -1)) #Average over space
+	sse = (u - v).pow(2).sum(dim=-3)
+	return sse.mean(dim=(-2, -1)) #Average over space
 
 def kld_loss(params, mu, logvar):
-    kld = mu.pow(2) + logvar.exp() - logvar - 1
-    kld = 0.5 * kld.sum(axis=-1).mean()
-    return kld
+	kld = mu.pow(2) + logvar.exp() - logvar - 1
+	kld = 0.5 * kld.sum(axis=-1).mean()
+	return kld
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('--num_latent', type=int, default=64)
-    parser.add_argument('--beta', type=float, default=1e-4)
-    parser.add_argument('--lr', type=float, default=1e-4)
-    parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--input', type=str, default='sqh')
-    parser.add_argument('--in_channels', type=int, default=4)
-    parser.add_argument('--output', type=str, default='vel')
-    parser.add_argument('--out_channels', type=int, default=2)
-    parser.add_argument('--batch_size', type=int, default=8)
-    parser.add_argument('--logdir', type=str, default='/project/vitelli/jonathan/REDO_fruitfly/tb_logs/May2024')
-    args = parser.parse_args()
+	parser = ArgumentParser()
+	parser.add_argument('--num_latent', type=int, default=64)
+	parser.add_argument('--beta', type=float, default=1e-4)
+	parser.add_argument('--lr', type=float, default=1e-4)
+	parser.add_argument('--epochs', type=int, default=100)
+	parser.add_argument('--input', type=str, default='sqh')
+	parser.add_argument('--in_channels', type=int, default=4)
+	parser.add_argument('--output', type=str, default='vel')
+	parser.add_argument('--out_channels', type=int, default=2)
+	parser.add_argument('--batch_size', type=int, default=8)
+	parser.add_argument('--logdir', type=str, default='/project/vitelli/jonathan/REDO_fruitfly/tb_logs/May2024')
+	args = parser.parse_args()
 
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    
-    '''
-    Build dataset
-    '''
-    print('Building datasets')
-    transform = Compose([
-        Reshape2DField(),
-        #LeftRightSymmetrize(),
-        RandomLR(),
-        ToTensor()
-    ])
+	device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+	
+	'''
+	Build dataset
+	'''
+	print('Building datasets')
+	transform = Compose([
+		Reshape2DField(),
+		RandomLR(),
+		ToTensor()
+	])
 
-    #Base datasets
-    dataset = torch.utils.data.ConcatDataset([
-        WTDataset(transform=transform),
-        TwistDataset(transform=transform),
-        TollDataset(transform=transform),
-        SpaetzleDataset(transform=transform),
-    ])
+	#Base datasets
+	dataset = torch.utils.data.ConcatDataset([
+		WTDataset(transform=transform),
+		TwistDataset(transform=transform),
+		#TollDataset(transform=transform),
+		#SpaetzleDataset(transform=transform),
+	])
 
-    # Split on embryos
-    full_df = pd.concat([ds.df for ds in dataset.datasets], ignore_index=True)
-    embryos = full_df.embryoID.unique()
-    train, val = train_test_split(embryos, test_size=0.5, random_state=42)
-    print('Train embryos: ', train)
-    print('Val embryos: ', val)
-    
-    # Find dataset indices for each embryo
-    train_idxs = full_df[full_df.embryoID.isin(train)].index.values
-    val_idxs = full_df[full_df.embryoID.isin(val)].index.values
-    train = Subset(deepcopy(dataset), train_idxs)
-    val = Subset(deepcopy(dataset), val_idxs)
-    print('Train size: ', len(train))
-    print('Val size: ', len(val))
-    
+	# Split on embryos
+	full_df = pd.concat([ds.df for ds in dataset.datasets], ignore_index=True)
+	embryos = full_df.embryoID.unique()
+	train, val = train_test_split(embryos, test_size=0.5, random_state=42)
+	print('Train embryos: ', train)
+	print('Val embryos: ', val)
+	
+	# Find dataset indices for each embryo
+	train_idxs = full_df[full_df.embryoID.isin(train)].index.values
+	val_idxs = full_df[full_df.embryoID.isin(val)].index.values
+	train = Subset(deepcopy(dataset), train_idxs)
+	val = Subset(deepcopy(dataset), val_idxs)
+	print('Train size: ', len(train))
+	print('Val size: ', len(val))
+	
 
-    train_loader = DataLoader(train, num_workers=4, batch_size=args.batch_size, shuffle=True, pin_memory=True)
-    val_loader = DataLoader(val, num_workers=4, batch_size=args.batch_size, shuffle=True, pin_memory=True)
+	train_loader = DataLoader(train, num_workers=4, batch_size=args.batch_size, shuffle=True, pin_memory=True)
+	val_loader = DataLoader(val, num_workers=4, batch_size=args.batch_size, shuffle=True, pin_memory=True)
 
-    '''
-    Build the model
-    '''
-    model = MaskedVAE(in_channels=args.in_channels,
-                      out_channels=args.out_channels,
-                      num_latent=args.num_latent,
-                      stage_dims=[[32,32],[64,64],[128,128],[256,256]])
+	'''
+	Build the model
+	'''
+	model = MaskedVAE(in_channels=args.in_channels,
+					  out_channels=args.out_channels,
+					  num_latent=args.num_latent,
+					  stage_dims=[[32,32],[64,64],[128,128],[256,256]])
 
-    model.to(device)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=20, factor=0.5, min_lr=1e-5)
-    
-    '''
-    Train the model
-    '''
-    
-    savename = f'{model.__class__.__name__}_{args.input}_beta={args.beta:.2g}_split=embryo'
-    print(savename)
+	model.to(device)
+	optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
+	scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=20, factor=0.5, min_lr=1e-5)
+	
+	'''
+	Train the model
+	'''
+	
+	savename = f'{model.__class__.__name__}_{args.input}_beta={args.beta:.2g}_split=embryo'
+	savename = f'{model.__class__.__name__}_{args.input}_beta={args.beta:.2g}_split=embryo_WTTwist'
+	print(savename)
 
-    best_res = 1e5
-    for epoch in range(args.epochs):
-        model.train()
-        train_loss = 0.
-        for batch in train_loader:
-            x = batch[args.input].to(device)
-            y0 = batch[args.output].to(device)
-            y, pl = model(x)
-            
-            kld = kld_loss(*pl)
-            #res = residual(y, y0).mean()
-            #loss = res + args.beta * kld
-            
-            mse = mean_squared_error(y, y0).mean()
-            loss = mse + args.beta * kld
-            
-            train_loss += loss.item()
-            
-            optimizer.zero_grad()
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-            optimizer.step()
+	best_res = 1e5
+	for epoch in range(args.epochs):
+		model.train()
+		train_loss = 0.
+		for batch in train_loader:
+			x = batch[args.input].to(device)
+			y0 = batch[args.output].to(device)
+			y, pl = model(x)
+			
+			kld = kld_loss(*pl)
+			#res = residual(y, y0).mean()
+			#loss = res + args.beta * kld
+			
+			mse = mean_squared_error(y, y0).mean()
+			loss = mse + args.beta * kld
+			
+			train_loss += loss.item()
+			
+			optimizer.zero_grad()
+			loss.backward()
+			torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+			optimizer.step()
 
-        model.eval()
-        val_loss = 0.
-        res_val, mse_val, kld_val = 0., 0., 0.
-        residuals, mses = [], []
-        
-        with torch.no_grad():
-            for batch in val_loader:
-                x = batch[args.input].to(device)
-                y0 = batch[args.output].to(device)
-                y, pl = model(x)
+		model.eval()
+		val_loss = 0.
+		res_val, mse_val, kld_val = 0., 0., 0.
+		residuals, mses = [], []
+		
+		with torch.no_grad():
+			for batch in val_loader:
+				x = batch[args.input].to(device)
+				y0 = batch[args.output].to(device)
+				y, pl = model(x)
 
-                kld = kld_loss(*pl)
-                res = residual(y, y0).mean()
-                mse = mean_squared_error(y, y0).mean()
+				kld = kld_loss(*pl)
+				res = residual(y, y0).mean()
+				mse = mean_squared_error(y, y0).mean()
 
-                loss = mse + args.beta * kld
-                val_loss += loss.item() / len(val_loader)
-                res_val += res.item() / len(val_loader)
-                mse_val += mse.item() / len(val_loader)
-                kld_val += kld.item() / len(val_loader)
+				loss = mse + args.beta * kld
+				val_loss += loss.item() / len(val_loader)
+				res_val += res.item() / len(val_loader)
+				mse_val += mse.item() / len(val_loader)
+				kld_val += kld.item() / len(val_loader)
 
-                residuals.append(res.item())
-                mses.append(mse.item())
+				residuals.append(res.item())
+				mses.append(mse.item())
 
-        scheduler.step(val_loss)
+		scheduler.step(val_loss)
 
-        res_val = np.mean(residuals)
-        
-        outstr  = f'Epoch {epoch:03d} Val Loss = {val_loss:.3f} '
-        outstr += f'Res = {res_val:.3f} MSE = {mse_val:.3f} KLD = {kld_val:.3f}'
-        print(outstr)
-        if res_val < best_res:
-            save_dict = {
-                'state_dict': model.state_dict(),
-                'hparams': vars(args),
-                'epoch': epoch,
-                'loss': val_loss,
-                'mse': np.mean(mses),
-                'mse_std': np.std(mses),
-                'res': np.mean(residuals),
-                'res_std': np.std(residuals),
-                'val_df': full_df.iloc[val.indices],
-            }
-            torch.save(save_dict, f'{args.logdir}/{savename}')
-            best_res = res_val
+		res_val = np.mean(residuals)
+		
+		outstr	= f'Epoch {epoch:03d} Val Loss = {val_loss:.3f} '
+		outstr += f'Res = {res_val:.3f} MSE = {mse_val:.3f} KLD = {kld_val:.3f}'
+		print(outstr)
+		if res_val < best_res:
+			save_dict = {
+				'state_dict': model.state_dict(),
+				'hparams': vars(args),
+				'epoch': epoch,
+				'loss': val_loss,
+				'mse': np.mean(mses),
+				'mse_std': np.std(mses),
+				'res': np.mean(residuals),
+				'res_std': np.std(residuals),
+				'val_df': full_df.iloc[val.indices],
+			}
+			torch.save(save_dict, f'{args.logdir}/{savename}')
+			best_res = res_val
